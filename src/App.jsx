8c4a45409fba1e8524dd882f8ae287f514190781
Shri Om Trivedi <tshriom@gmail.com>
2023-04-23 20:10:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import DetectCircle from './questions-answers/DetectCircle';
+
+function App() {
+  return (
+    <div>
+      Frontend Interview Questions
+      <div>
+        <DetectCircle />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/questions-answers/DetectCircle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DetectCircle = () => {
+  return (
+    <div>DetectCircle</div>
+  )
+}
+
+export default DetectCircle
